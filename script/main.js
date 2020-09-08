662926dbcc6b4959f39e423d6364c7ae8d059df4
Sizon <sizon@vidrada.org>
2020-09-08 15:44:52 +0300

    add js function + object

--- script/main.js
+++ script/main.js
@@ -1,3 +1,53 @@
-const score = document.querySelector('.score'),
-  start = document.querySelector('.start'),
-  gameArea = document.querySelector('.game-area');
+const score = document.querySelector(".score"),
+  start = document.querySelector(".start"),
+  gameArea = document.querySelector(".game-area"),
+  car = document.createElement("div");
+
+  car.classList.add("car");
+
+// Object
+const keys = {
+  ArrowUp: false,
+  ArrowDown: false,
+  ArrowRight: false,
+  ArrowLeft: false,
+};
+
+const setting = {
+  start: false,
+  score: 0,
+  speed: 3,
+};
+
+// Function
+const startGame = () => {
+  start.classList.add("hide");
+  setting.start = true;
+  gameArea.appendChild(car);
+  requestAnimationFrame(playGame);
+};
+
+const playGame = () => {
+  console.log("playGame: ", playGame);
+
+  if (setting.start) {
+    requestAnimationFrame(playGame);
+  }
+};
+
+const startRun = (event) => {
+  const key = event.key;
+  event.preventDefault();
+  keys[key] = true;
+};
+
+const stopRun = (event) => {
+  const key = event.key;
+  event.preventDefault();
+  keys[key] = false;
+};
+
+// Event Listener
+start.addEventListener("click", startGame);
+document.addEventListener("keydown", startRun);
+document.addEventListener("keyup", stopRun);
